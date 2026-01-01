75b1311ea92797fa1bcf4760ab7e43b96b3c88c2
Steven Rudenko <srudenko@ncl.com>
2026-01-02 00:21:23 +0300

    imporve math interp test

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -14,18 +14,22 @@ OPS = ["+", "-", "*", "/"]
             st.integers(min_value=0, max_value=3),
             st.integers(min_value=1, max_value=3),
         ),
-        min_size=2,
+        min_size=1,
     )
 )
 def test_intepreter_math(int_op: list[tuple[int, int, int]]):
-    inp = [str(int_op[0][0])]
     opened_p = 0
+    inp = []
+    if int_op[0][2] == 1:
+        inp = ["("]
+        opened_p = 1
+    inp.append(str(int_op[0][0]))
     for i in range(0, len(int_op) - 1):
-        if opened_p > 0 and int_op[i][2] == 3:
+        if opened_p > 0 and int_op[i + 1][2] == 3:
             inp.append(")")
             opened_p -= 1
         inp.append(OPS[int_op[i][1]])
-        if int_op[i][2] == 2:
+        if int_op[i + 1][2] == 2:
             inp.append("(")
             opened_p += 1
         inp.append(str(int_op[i + 1][0]))
@@ -34,7 +38,6 @@ def test_intepreter_math(int_op: list[tuple[int, int, int]]):
     code = "".join(inp)
     lexer = Lexer(code)
     interpreter = Interpreter(lexer)
-    print(code)
     try:
         result = eval(code.replace("/", "//"))
     except ZeroDivisionError:
