0fd02b2b4d4b0c4fc2038a3a9c579038c6d91be3
Steven Rudenko <srudenko@ncl.com>
2026-01-09 04:41:03 +0300

    add first fibonacci script

---
+++ examples/fibonacci.pas
@@ -0,0 +1,13 @@
+program fib_1;
+    var res: integer;
+
+    function fib(num: integer): integer;
+    begin
+        if (num <= 1) then
+            exit(num);
+        exit(fib(num - 1) + fib(num - 2))
+    end;
+
+begin
+    res := fib(9);
+end.
